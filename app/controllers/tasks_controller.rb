4239d4a0ba605e5449ce3caeed142f977c9ace31
rocionadal <rocioonadal@gmail.com>
2020-11-24 16:14:01 +0100

    update routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save!
 
-    redirect_to show_task_path(@task)
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     @task.update(task_params)
-    redirect_to show_task_path(@task)
+    redirect_to task_path(@task)
   end
 
   def destroy
